952c3cbd03928199646aae39bec251aeca042712
Matthias Koch <ithrowexceptions@gmail.com>
2019-01-06 17:44:06 +0100

    Remove IRunConfigurationWithDefault interface

--- src/rider/main/kotlin/com/jetbrains/rider/plugins/nuke/NukeBuildTargetConfigurationType.kt
+++ src/rider/main/kotlin/com/jetbrains/rider/plugins/nuke/NukeBuildTargetConfigurationType.kt
@@ -1,23 +1,17 @@
 package com.jetbrains.rider.plugins.nuke
 
-import com.intellij.execution.RunManager
-import com.intellij.execution.RunnerAndConfigurationSettings
 import com.intellij.execution.configurations.ConfigurationTypeBase
-import com.intellij.openapi.project.Project
-import com.jetbrains.rider.model.RunnableProject
 import com.jetbrains.rider.model.RunnableProjectKind
 import com.jetbrains.rider.run.configurations.IRunnableProjectConfigurationType
-import com.jetbrains.rider.run.configurations.IRunConfigurationWithDefault
 import com.jetbrains.rider.run.configurations.project.DotNetProjectConfigurationFactory
 
 class NukeBuildTargetConfigurationType
     : ConfigurationTypeBase(
         "NukeBuildTarget",
         "NUKE",
-        "NUKE target execution configuration",
+        "NUKE",
     NukeIcons.Icon),
-        IRunnableProjectConfigurationType,
-        IRunConfigurationWithDefault {
+        IRunnableProjectConfigurationType {
 
     private val factoryConfiguration: DotNetProjectConfigurationFactory = DotNetProjectConfigurationFactory(this)
 
@@ -25,11 +19,6 @@ class NukeBuildTargetConfigurationType
         addFactory(factoryConfiguration)
     }
 
-    override val priority: IRunConfigurationWithDefault.Priority
-        get() = IRunConfigurationWithDefault.Priority.Top
-
     override fun isApplicable(kind: RunnableProjectKind) =
-            kind == RunnableProjectKind.Console || kind == RunnableProjectKind.DotNetCore
-
-    override fun tryCreateDefault(projects: List<RunnableProject>, project: Project, runManager: RunManager) = null
+            kind == RunnableProjectKind.Console
 }
