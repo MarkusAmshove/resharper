025261dd88e2cdf30ba3e9d847d79dad673d0c29
Matthias Koch <ithrowexceptions@gmail.com>
2019-01-07 17:13:53 +0100

    Update build

--- settings.ps1
+++ settings.ps1
@@ -1,12 +1,11 @@
 $PSScriptRoot = Split-Path $MyInvocation.MyCommand.Path -Parent
-$PluginId = "ReSharper.Nuke"
-$SolutionPath = "$PSScriptRoot\nuke-resharper.sln"
+$PluginId = "ReSharperPlugin.SamplePlugin"
+$SolutionPath = "$PSScriptRoot\SamplePlugin.sln"
 $SourceBasePath = "$PSScriptRoot\src\dotnet"
 
-$VisualStudioBaseDirectory = "${env:ProgramFiles(x86)}\Microsoft Visual Studio\2017\*\"
-if (@(Get-ChildItem "$VisualStudioBaseDirectory").Count -ne 1) { throw "Could not find single VisualStudio base directory. Please adjust the search pattern. " }
+$VisualStudioBaseDirectory = & "$PSScriptRoot\tools\vswhere.exe" "-latest" "-property" "installationPath"
 $DevEnvPath = Get-ChildItem "$VisualStudioBaseDirectory\Common7\IDE\devenv.exe"
-$MSBuildPath = Get-ChildItem "$VisualStudioBaseDirectory\MSBuild\15.0\Bin\MSBuild.exe"
+$MSBuildPath = Get-ChildItem "$VisualStudioBaseDirectory\MSBuild\*\Bin\MSBuild.exe"
 
 $OutputDirectory = "$PSScriptRoot\output"
 $NuGetPath = "$PSScriptRoot\tools\nuget.exe"
@@ -19,7 +18,9 @@ Function Invoke-Exe {
     )
 
     Write-Host "> $Executable $Arguments"
-    $rc = Start-Process -FilePath $Executable -ArgumentList $Arguments -NoNewWindow -Wait -Passthru
+    $rc = Start-Process -FilePath $Executable -ArgumentList $Arguments -NoNewWindow -Passthru
+    $rc.Handle # to initialize handle according to https://stackoverflow.com/a/23797762/2684760
+    $rc.WaitForExit()
     if (-Not $ValidExitCodes.Contains($rc.ExitCode)) {
         throw "'$Executable $Arguments' failed with exit code $($rc.ExitCode), valid exit codes: $ValidExitCodes"
     }
